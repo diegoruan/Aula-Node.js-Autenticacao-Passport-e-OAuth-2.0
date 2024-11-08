22d260773e0ffe01f67a4fea82ae8974dcb50425
Diego Ruan <diego_ruan.14@hotmail.com>
2024-11-08 11:07:47 -0300

    Aula 1 - iniciando o projeto

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const getDb = require('../util/database').getDb
+
+class User {
+    constructor(username, email, password) {
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    async save() {
+        const db = getDb()
+        return db.collection('users').insertOn(this)
+    }
+}
+module.exports = User
