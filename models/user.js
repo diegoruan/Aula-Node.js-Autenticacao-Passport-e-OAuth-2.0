80ec09c0dee60c7d1fc559aedda880f4478fe89e
Diego Ruan <diego_ruan.14@hotmail.com>
2024-11-08 14:50:59 -0300

    Aula 2 - Login e banco de dados

--- models/user.js
+++ models/user.js
@@ -9,7 +9,13 @@ class User {
 
     async save() {
         const db = getDb()
-        return db.collection('users').insertOn(this)
+        return db.collection('users').insertOne(this)
+    }
+
+    static async findOne(email, password) {
+        const db = getDb()
+        const user = await db.collection('users').findOne({email: email, password: password})
+        return user
     }
 }
 module.exports = User
